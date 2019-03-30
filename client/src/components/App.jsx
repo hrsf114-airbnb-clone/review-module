91daf5e17006ed49b0ab85a2a570746253c50577
johnjaynichols <johnjaynichols@gmail.com>
2019-03-29 19:47:37 -0700

    Modify front-end elements.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
 const App = () => (
+  <div>
   <h1>Reviews</h1>
+  <img src="../star-filled.png"></img>
+  <ul>
+    <li>
+      Review text
+    </li>
+  </ul>
+  </div>
 );
 
 export default App;
